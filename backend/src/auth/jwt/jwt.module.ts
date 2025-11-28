dae47458cde66a677f3ee0327f37bbe1a965a838
Angelo Russo <rxssbusiness@gmail.com>
2025-11-28 21:49:00 +0100

    feat(auth): register jwt module

--- backend/src/auth/jwt/jwt.module.ts
+++ backend/src/auth/jwt/jwt.module.ts
@@ -1,9 +1,19 @@
 import { Module } from "@nestjs/common";
 import { JwtAuthService } from "./jwt.service";
+import { JwtModule } from "@nestjs/jwt";
+import { UserModule } from "src/user/user.module";
 
 @Module({
+    imports: [
+        UserModule,
+        JwtModule.register({
+            global: true,
+            secret: `${process.env.JWT_SECRET}`,
+            signOptions: { expiresIn: '60s' },
+        }),
+    ],
     providers: [JwtAuthService],
     exports: [JwtAuthService]
 })
 
-export class JwtAuthModule {}
+export class JwtAuthModule { }
