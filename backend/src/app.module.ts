eac4d092ad20691ee989a0ee6358102390b6b0a2
Angelo Russo <rxssbusiness@gmail.com>
2025-11-28 21:46:57 +0100

    feat(app): swagger and app

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,20 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from 'prisma/prisma.service';
+import { UserService } from './user/user.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    UserModule,
+    AuthModule
+  ],
+  providers: [
+    PrismaService, UserService
+  ]
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST'],
+    allowedHeaders: 'Content-Type,Authorization',
+  })
+
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Task Manager')
+    .setDescription('Manage your tasks')
+    .setVersion('1.0')
+    .addTag('taskmanager')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
