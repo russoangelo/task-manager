bb92a6a6f71215febff7ebdb26b743201565e12e
Angelo Russo <rxssbusiness@gmail.com>
2025-11-28 21:52:49 +0100

    fix(prisma): update prisma service

--- backend/prisma/prisma.service.ts
+++ backend/prisma/prisma.service.ts
@@ -1,9 +1,13 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-  async onModuleInit() {
-    await this.$connect();
+export class PrismaService extends PrismaClient {
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL as string,
+    });
+    super({ adapter });
   }
 }
