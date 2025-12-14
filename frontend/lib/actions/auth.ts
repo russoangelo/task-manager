96982babcea7a65529efa84873587d8ce1bcc238
Angelo Russo <rxssbusiness@gmail.com>
2025-12-14 21:15:36 +0100

    feat: signin action

--- frontend/lib/actions/auth.ts
+++ frontend/lib/actions/auth.ts
@@ -1,6 +1,7 @@
 'use server'
 
-import { signUpSchema } from "../zod/auth";
+import { cookies } from "next/headers";
+import { signInSchema, signUpSchema } from "../zod/auth";
 import { actionClient } from "./safe-action";
 
 export const signUpAction = actionClient
@@ -15,9 +16,6 @@ export const signUpAction = actionClient
                 body: JSON.stringify(parsedInput)
             })
 
-            console.log('Response status:', response.status)
-            console.log('Response ok:', response.ok)
-
             if (!response.ok) {
                 throw new Error("invalid-credentials")
             }
@@ -28,6 +26,51 @@ export const signUpAction = actionClient
                 success: true,
                 user: data.email
             }
+        } catch (error) {
+            throw new Error(error instanceof Error ? error.message : "network-error")
+        }
+    })
+
+export const signInAction = actionClient
+    .schema(signInSchema)
+    .action(async ({ parsedInput }) => {
+        try {
+            const response = await fetch(`${process.env.BACKEND_LOCATION}/api/auth/login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(parsedInput)
+            })
+
+            if (!response.ok) {
+                throw new Error("invalid-credentials")
+            }
+
+            const data = await response.json()
+
+            const { accessToken, refreshToken } = data;
+
+            const cookieStore = await cookies()
+
+            cookieStore.set('access_token', accessToken, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: 'lax',
+                path: '/',
+                maxAge: 60 * 15,
+            });
+
+            cookieStore.set('refresh_token', refreshToken, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: 'lax',
+                path: '/',
+                maxAge: 60 * 60 * 24 * 7,
+            });
+
+            return { success: true }
+
         } catch (error) {
             throw new Error(error instanceof Error ? error.message : "network-error")
         }
